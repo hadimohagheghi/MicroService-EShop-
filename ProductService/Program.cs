808c867fb8cd7d80e4a75177955c47862d5ef97f
Hadi Mohagheghi <hadimohagheghi@live.com>
2025-10-04 19:16:07 +0330

    ایجاد DbContext ConnectionString افزودن سرویس AddDbContext به سرویس ها

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using ProductService.Infrastructure.Contexts;
+
 namespace ProductService
 {
     public class Program
@@ -13,6 +16,10 @@ namespace ProductService
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<ProductDatabaseContext>(p =>
+                p.UseSqlServer(builder.Configuration["ProductConnection"]));
 
             var app = builder.Build();
 
