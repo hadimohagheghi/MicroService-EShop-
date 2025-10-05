cf9e738cae721cac79b30fa80c643bb66efa53df
Hadi Mohagheghi <hadimohagheghi@live.com>
2025-10-05 12:29:44 +0330

    اضافه کردن سرویس های مورد نیاز سبدخرید و استفاده از AutoMapper در سرویس

---
+++ BasketService/Model/Dto/AddItemToBasketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BasketService.Model.Dto;
+
+public class AddItemToBasketDto
+{
+    public Guid basketId { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int UnitPrice { get; set; }
+    public int Quantity { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ BasketService/Model/Services/Interface/IBasketService.cs
@@ -0,0 +1,20 @@
+﻿using BasketService.Model.Dto;
+
+namespace BasketService.Model.Services.Interface
+{
+    public interface IBasketService
+    {
+        /// <summary>
+        /// نمایش سبد خرید به کاربر و اگر سبد وجود نداشت براش ایجاد کنه
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns>اطلاعات سبد رو برای کاربر ارسال می کنیم</returns>
+        BasketDto GetOrCreateBasketForUser(string UserId);
+        BasketDto GetBasket(string UserId);
+        void AddItemToBasket(AddItemToBasketDto item);
+        void RemoveItemFromBasket(Guid ItemId);
+        void SetQuantities(Guid itemId, int quantity);
+        void TransferBasket(string anonymousId, string UserId);
+
+    }
+}
