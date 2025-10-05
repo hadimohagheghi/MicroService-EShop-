d1001299e2bd5c614be4b52c6dc449477330083f
Hadi Mohagheghi <hadimohagheghi@live.com>
2025-10-05 11:43:25 +0330

    ایجاد Entity های Basket و  BasketItem

---
+++ BasketService/Model/Entities/Basket.cs
@@ -0,0 +1,18 @@
+﻿namespace BasketService.Model.Entities
+{
+    public class Basket
+    {
+        //بعداً برای کاربران ناشناس هم یک شناسه کاربری ایجاد می‌کنیم 
+        public Basket(string UserId)
+        {
+            this.UserId = UserId;
+        }
+        public Basket()
+        {
+        }
+        public Guid Id { get; set; }
+        public string UserId { get; private set; }
+
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
